7d7ddb9717a44f889ce20fa1f2d1e3d186cb4c6b
Eren ERGEN <erenergen96@icloud.com>
2023-08-14 14:30:44 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/rocognize/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.rocognize
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/views/camera_view.dart
@@ -0,0 +1,21 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:rocognize/controller/scan_controller.dart';
+
+class CameraView extends StatelessWidget {
+  const CameraView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GetBuilder<ScanController>(
+          init: ScanController(),
+          builder: (controller) {
+            return controller.isCameraInitialized.value
+                ? CameraPreview(controller.cameraController)
+                : Center(child: Text("Loading Preview..."));
+          }),
+    );
+  }
+}
